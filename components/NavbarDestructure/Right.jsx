8b421f4492a61f3acd85cc027d2fe9dbbd826c09
Hasmukh <hasmukhsingh90@gmail.com>
2023-10-17 13:52:33 +0530

    Last initial commit

--- components/NavbarDestructure/Right.jsx
+++ components/NavbarDestructure/Right.jsx
@@ -1,5 +1,3 @@
-
-
 "use client";
 import { BsFillCartFill } from "react-icons/bs";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -17,7 +15,9 @@ const Right = () => {
 
   return (
     <div className={``}>
-      <div className={`flex items-center justify-between sm:justify-center gap-5 sm:gap-4  hidden sm:flex`}>
+      <div
+        className={`flex items-center justify-between sm:justify-center gap-5 sm:gap-4  hidden sm:flex`}
+      >
         <button>Register</button>
         <button>Sign up</button>
         <BsFillCartFill size={16} />
@@ -28,30 +28,40 @@ const Right = () => {
           className={`relative transition-transform transform rotate-0 hover:rotate-45`}
           onClick={toggleHamburger}
         />
-        <div className={`w-full h-[100vh] bg-zinc-900 left-0 z-10 fixed top-0 px-[6vw] py-[6vw]`}>
+        <div
+          className={`w-full h-[100vh] bg-zinc-900 left-0 z-10 fixed top-0 px-[6vw] py-[6vw]`}
+        >
           <div className={`w-full flex flex-col`}>
             <div className={`flex justify-between items-center`}>
               <h1 className={`font-bold text-2xl text-white`}>Quick Links</h1>
               <CloseIcon className={`text-white`} onClick={toggleHamburger} />
             </div>
-            <div className={`flex flex-col gap-10 mt-7 text-center h-[80vh] py-[4vw] items-center justify-center relative`}>
+            <div
+              className={`flex flex-col gap-10 mt-7 text-center h-[80vh] py-[4vw] items-center justify-center relative`}
+            >
               <Link href={`/`} className={`text-4xl font-semibold text-white`}>
                 Home
               </Link>
               <Link href={""} className={`text-4xl font-semibold text-white`}>
                 About
               </Link>
-              <Link href={""} className ={`text-4xl font-semibold text-white`}>
+              <Link href={""} className={`text-4xl font-semibold text-white`}>
                 Explore
               </Link>
               <Link href={""} className={`text-4xl font-semibold text-white`}>
                 Contact
               </Link>
-              <div className={`flex justify-between w-full p-[4vw] absolute bottom-0`}>
-                <button className={`bg-slate-400 px-[5vw] py-[3vw] rounded-3xl text-white`}>
+              <div
+                className={`flex justify-between w-full p-[4vw] absolute bottom-0`}
+              >
+                <button
+                  className={`bg-slate-400 px-[5vw] py-[3vw] rounded-3xl text-white`}
+                >
                   Register
                 </button>
-                <button className={`bg-slate-400 px-[5vw] rounded-3xl py-[3vw] text-white`}>
+                <button
+                  className={`bg-slate-400 px-[5vw] rounded-3xl py-[3vw] text-white`}
+                >
                   Login
                 </button>
               </div>
@@ -62,7 +72,7 @@ const Right = () => {
 
       {/* Show the MenuIcon only on small devices */}
       <MenuIcon
-        className={`md:hidden transition-transform transform rotate-0 hover:rotate-45 ${hamburger ? "hidden" : ""}`}
+        className={`md:hidden transition-transform transform rotate-0 hover:rotate-45`}
         onClick={toggleHamburger}
       />
     </div>
@@ -70,5 +80,3 @@ const Right = () => {
 };
 
 export default Right;
-
-
